096efa415f6bb3e27e9bb85f809fca0570441f48
kosi <hugo-pioda_student2020@wilder.school>
2024-07-10 10:26:33 +0200

    add rigidbody to cube

--- src/Ball.jsx
+++ src/Ball.jsx
@@ -39,7 +39,6 @@ export default function Ball() {
               );
             }
 
-            console.log(target)
           }}
     >
         <mesh castShadow>
--- src/Cube.jsx
+++ src/Cube.jsx
@@ -1,7 +1,11 @@
 import * as THREE from 'three'
 import { useGLTF } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
+import { useRef } from 'react'
 
 export default function Cube() {
+    const cube = useRef()
+
     const boxMaterial = new THREE.MeshStandardMaterial({
         color: '#459E15',
         metalness: '0.5',
@@ -18,44 +22,54 @@ export default function Cube() {
     const { nodes } = useGLTF('./meshes/cube.glb')
 
     return (
-        <group scale={[0.2, 0.2, 0.2]}>
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Top.geometry}
-                material={boxMaterial}
-            />
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Level1.geometry}
-                material={levelMaterial}
-            />
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Level2.geometry}
-                material={levelMaterial}
-            />
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Level3.geometry}
-                material={levelMaterial}
-            />
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Level4.geometry}
-                material={levelMaterial}
-            />
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.Bottom.geometry}
-                material={boxMaterial}
-            />
-        </group>
+        <RigidBody
+            ref={cube}
+            type="kinematicPosition"
+            colliders="trimesh"
+            name="level"
+            position={[0, 0, 0]}
+            restitution={0.2}
+            friction={0}
+        >
+            <group scale={[0.2, 0.2, 0.2]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Top.geometry}
+                    material={boxMaterial}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Level1.geometry}
+                    material={levelMaterial}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Level2.geometry}
+                    material={levelMaterial}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Level3.geometry}
+                    material={levelMaterial}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Level4.geometry}
+                    material={levelMaterial}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Bottom.geometry}
+                    material={boxMaterial}
+                />
+            </group>
+        </RigidBody>
     )
 }
 
