12f3cacaf1a4dd6a74f9a06a04d71fc0bd7a10b8
kosi <hugo-pioda_student2020@wilder.school>
2024-11-01 20:18:01 +0100

    add button - new phase

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -1,27 +1,52 @@
-import { useProgress } from '@react-three/drei'
 import { useEffect, useState } from 'react'
+import { useProgress } from '@react-three/drei'
 import useGame from './store/useGame.js'
 
 export default function Overlay() {
     const [isLoaded, setIsLoaded] = useState(false)
-    const imagesLoaded = useGame((state) => state.imagesLoaded)
 
-    const { progress } = useProgress()
-    
+    /**
+     * Handle assets loading
+     */
+    const { progress } = useProgress() // loading for meshes
+    const imagesLoaded = useGame((state) => state.imagesLoaded) // loading for map images
+    const intro = useGame((state) => state.intro) // set phase to intro
+
+    const [loadingProgress, setLoadingProgress] = useState(25)
+    const [hasIncrementedImagesLoaded, setHasIncrementedImagesLoaded] = useState(false)
+    const [hasIncrementedProgress, setHasIncrementedProgress] = useState(false)
+
     useEffect(() => {
-        if (progress === 100 && imagesLoaded === true) {
-            const timer = setTimeout(() => {
-                setIsLoaded(true)
-            }, 500)
-
-            return () => clearTimeout(timer)
+        if (imagesLoaded && !hasIncrementedImagesLoaded) {
+            setLoadingProgress((prev) => prev + 25)
+            setHasIncrementedImagesLoaded(true)
         }
-    }, [progress, imagesLoaded])
+    }, [imagesLoaded, hasIncrementedImagesLoaded])
+
+    useEffect(() => {
+        if (progress === 100 && !hasIncrementedProgress) {
+            setLoadingProgress((prev) => prev + 50)
+            setHasIncrementedProgress(true)
+        }
+    }, [progress, hasIncrementedProgress])
+
+    /**
+     * Btn Start
+     */
+    const handleButtonClick = () => {
+        if (loadingProgress === 100) {
+            setIsLoaded(true)
+            intro()
+        }
+    }
 
     return (
         <div className={`overlay${isLoaded ? ' loaded' : ''}`}>
+            <button className="btn" onClick={handleButtonClick}>
+                Cliquez ici
+            </button>
             <div className="loader">
-                <p>{progress} % loaded</p>
+                <p>{loadingProgress} % loaded</p>
             </div>
         </div>
     )
